ca1ab40581418a5e12f760e34b6f2b828ddcc347
TB8466 <thorbjornbros@gmail.com>
2022-12-23 11:20:07 +0100

    GetContactByQuery, Careful of Rate-Limit / Secondly-limit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import getContacts from "./routers/getContacts.js";
 import * as dotenv from "dotenv"
 
@@ -7,11 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080 || 3000
 
-/* app.use(getContactsByQuery) */
+app.use(cors())
 
-app.get("/test", async (req, res) => {
-    res.send(await getContacts())
-})
+app.use(getContacts);
 
 app.listen(PORT, () => {
     console.log("Server is running on PORT:",PORT);
--- routers/getContacts.js
+++ routers/getContacts.js
@@ -1,31 +1,37 @@
 import { Client } from "@hubspot/api-client";
 import * as dotenv from "dotenv"
-dotenv.config()
+import { Router } from "express";
+
+const router = Router();
+dotenv.config();
 const hubspotClient = new Client({ "accessToken": process.env.ACCESS_TOKEN });
 
-
+router.get("/contact/:number", async (req, res) => {
   const PublicObjectSearchRequest = {
-    "filterGroups":[{"filters":[{"value":"*2222","propertyName":"phone","operator":"CONTAINS_TOKEN"}]}], 
+    "filterGroups":[{"filters":[{"value":`*${req.params.number}`,"propertyName":"phone","operator":"CONTAINS_TOKEN"}]}], 
     "sorts" : [
-     {
-       "propertyName" : "createdate",
-       "direction" : "DESCENDING"
-     }
-   ], 
-   "properties": ["firstname","lastname","phone", "email"],
-   "limit": 10,
- };
-
-
- export default async function getContactsByQuery() {
+      {
+        "propertyName" : "createdate",
+        "direction" : "DESCENDING"
+      }
+    ], 
+    "properties": ["firstname","lastname","phone", "email"],
+    "limit": 10,
+  };
   try {
     const apiResponse = await hubspotClient.crm.contacts.searchApi.doSearch(PublicObjectSearchRequest);
       
-      return JSON.parse(JSON.stringify(apiResponse.results, null, 2));
+    res.send(JSON.parse(JSON.stringify(apiResponse.results, null, 2)));
     
   } catch (e) {
     e.message === 'HTTP request failed'
       ? console.error(JSON.stringify(e.response, null, 2))
       : console.error(e)
   }
-}
+})
+
+
+
+
+
+export default router;
